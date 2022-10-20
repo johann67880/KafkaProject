6f5633c1763029132ff44dc5467ac3da5a2a104f
Johann Alberto Jimenez Caraballo <johaljicar@hotmail.com>
2022-10-19 22:38:17 -0500

    project
    initial project with basic consumer and producer

---
+++ KafkaProject/KafkaProject/Program.cs
@@ -0,0 +1,60 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KafkaProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        public async Task Producer(string message)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "host1:9092,host2: 9092",
+                ClientId = Dns.GetHostName()
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                await producer.ProduceAsync("testTopic", new Message<Null, string> { Value = message });
+            }
+        }
+
+        public async Task Consumer(string topics)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "host1:9092,host2:9092",
+                GroupId = "groupTest",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using(var builder = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                builder.Subscribe(topics);
+                var cancellationToken = new CancellationTokenSource();
+
+                try
+                {
+                    while(!cancellationToken.IsCancellationRequested)
+                    {
+                        var consumer = builder.Consume(cancellationToken.Token);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+                    builder.Close();
+                }
+            };
+        }
+    }
+}
